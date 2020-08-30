ba4d17b4d0019a4ef6c11d79e5ef8ebedfbc2837
Ronaldo <rfeitosa@ciandt.com>
2020-08-29 21:43:26 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod config;
+mod handlers;
+
+use color_eyre::Result;
+use crate::config::Config;
+use actix_web::{App, HttpServer, middleware::Logger};
+use tracing::info;
+use handlers::app_config;
+
+#[actix_rt::main]
+async fn main() -> Result<()> {
+    
+    let config = Config::from_env()
+        .expect("Server configuration");
+
+        info!("Starting server at http://{}:{}/", config.host, config.port);
+
+        HttpServer::new(|| {
+            App::new()
+                .wrap(Logger::default())
+                .configure(app_config)
+        })
+        .bind(format!("{}:{}", config.host, config.port))?
+        .run()
+        .await?;
+
+    Ok(())
+}
